0ad44fe418bf71190490f52174216f566117538c
Pavel Chuvashev <pavel.chuvashev@softwearfinance.com>
2012-12-06 13:15:54 +0400

    update button

--- 03-Development/00-Static/scripts/jquery.plugin.button.js
+++ 03-Development/00-Static/scripts/jquery.plugin.button.js
@@ -1,200 +1,235 @@
 /**
  * jQ button plugin
  * $(id).button();
- */
-
-/**
- * jQ setDisable plugin
- * $(id).setDisable(params);
- * @param {Boolean} isOnFocus flag disable
+ * @param {String} method name (default - init)
  */
 
 (function($){
-    var $d = $().$d;
+    var $d = $().$d,
+        methods = {
+            init: function() {
 
-    $.fn.button = function() {
+                return this.each(function() {
+                    var $this = $(this), // realInput
+                        wrapper = $this.parent(), //visualInput
+                        buttonDom = $this[0],
+                        form,
+                        action,
+                        eventAction,
+                        isPressed = false,
+                        isFocused = false,
+                        isHover = false,
+                        isDisabled = false,
+                        isKeyDown = false;
 
-        return this.each(function () {
-            var $this = $(this),
-                wrapper = $this.parent(),
-                buttonDom = $this[0],
-                form,
-                action,
-                eventAction,
-                isPressed = false,
-                isFocused = false,
-                isHover = false,
-                isDisabled = false,
-                isKeyDown = false;
-
-//            check default values
-            if (buttonDom.disabled) {
-                wrapper.addClass('isDisabled');
-            } else {
-                wrapper.removeClass('isDisabled');
-            }
-
-//            action
-            if ((buttonDom.tagName == 'A') && (buttonDom.href.length > 0)){
-                if (buttonDom.target != '') {
-                    action = function(){
-                        window.open(buttonDom.href, buttonDom.target);
-                    };
-                }
-                else if (buttonDom.target === '') {
-                    action = function(){
-                        window.location.href = buttonDom.href;
-                    };
-                }
-            } else if((buttonDom.tagName == 'INPUT') && (buttonDom.type == 'submit')){
-                form = $this.closest('form')[0];
-                if (form !== undefined) {
-                    action = function(){
-                        form.submit();
+//                    check default values
+                    if (buttonDom.disabled || (buttonDom.getAttribute('disabled') === 'disabled')) {
+                        wrapper.addClass('isDisabled');
+                        blur();
+                        isDisabled = false;
+                    } else {
+                        wrapper.removeClass('isDisabled');
+                        isDisabled = true;
                     }
-                }
-            } else {
-                action = function(){
-                    eventAction = $.Event('click');
-                    $this.trigger(eventAction);
-                }
-            }
 
-//            key-function
-            function keyDown(e, code){
-                if ((code == 13) || (code == 32)){
-                    e.preventDefault();
-                    e.stopPropagation();
-                    isKeyDown = true;
-                    wrapper.addClass('isPressed');
-                    action();
-                }
-            }
+//                    action
+                    if ((buttonDom.tagName == 'A') && (buttonDom.href.length > 0)){
+                        if (buttonDom.target != '') {
+                            action = function(){
+                                window.open(buttonDom.href, buttonDom.target);
+                            };
+                        }
+                        else if (buttonDom.target === '') {
+                            action = function(){
+                                window.location.href = buttonDom.href;
+                            };
+                        }
+                    } else if((buttonDom.tagName == 'INPUT') && (buttonDom.type == 'submit')){
+                        form = $this.closest('form')[0];
+                        if (form !== undefined) {
+                            action = function(){
+                                form.submit();
+                            }
+                        }
+                    } else {
+                        action = function(){
+                            eventAction = $.Event('click');
+                            $this.trigger(eventAction);
+                        }
+                    }
 
-            function operaKeyPress(e){
-                keyDown(e, e.keyCode);
-            }
-            function fxKeyPress(e){
-                keyDown(e, e.which);
-            }
-            function commonKeyDown(e){
-                if ((e.keyCode == 9) && ($d.isIE && ($d.browserVersion < 9))){
-                    if (!buttonDom.disabled) {
-                        wrapper.removeClass('isFocused').removeClass('isPressed');
-                        isFocused = false;
+//                    events
+                    function focus(){
+                        wrapper.addClass('isFocused');
+                        isFocused = true;
                     }
-                }
-                keyDown(e, e.keyCode);
-            }
-            function keyUp(e){
-                if ((e.keyCode == 13) || (e.keyCode == 32)){
-                    e.preventDefault();
-                    e.stopPropagation();
-                    isKeyDown = false;
-                    if (!isPressed){
-                        wrapper.removeClass('isPressed');
-                    } else if (!isHover){
-                        wrapper.removeClass('isPressed');
+                    function blur(){
+                        wrapper.blur();
+                        wrapper.removeClass('isFocused')
+                               .removeClass('isPressed');
+                        isPressed = isKeyDown = isFocused = false;
                     }
-                }
-            }
-
-            wrapper.on({
-//                touch
-                touchstart: function(e) {
-                    if (buttonDom.getAttribute('disabled') !== 'disabled'){
-                        e.preventDefault();
-                        isPressed = true;
-                        wrapper.addClass('isPressed');
-                    }
-                },
-                touchend: function(){
-                    wrapper.removeClass('isPressed');
-                    isPressed = false;
-                    action();
-                },
-//                hover
-                mouseenter: function() {
-                    if (buttonDom.getAttribute('disabled') !== 'disabled'){
-                        isHover = true;
-                        wrapper.addClass('isHover');
-                    }
-                },
-                mouseleave: function() {
-                    isHover = false;
-                    wrapper.removeClass('isHover');
-                },
-//                pressed
-                mousedown: function(e) {
-                    if (buttonDom.getAttribute('disabled') !== 'disabled'){
-                        e.preventDefault();
-                        isPressed = true;
-                        wrapper.addClass('isPressed');
-                        wrapper.mouseleave(function() {
+                    function removePress(){
+                        if (!isKeyDown){
                             wrapper.removeClass('isPressed');
+                        }
+                    }
+
+//                    key-function
+                    function keyDown(e, code){
+                        if ((code == 13) || (code == 32)){
+                            e.preventDefault();
+                            e.stopPropagation();
+                            isKeyDown = true;
+                            wrapper.addClass('isPressed');
+                            action();
+                        }
+                    }
+                    function operaKeyPress(e){
+                        keyDown(e, e.keyCode);
+                    }
+                    function fxKeyPress(e){
+                        keyDown(e, e.which);
+                    }
+                    function commonKeyDown(e){
+                        if ((e.keyCode == 9) && ($d.isIE && ($d.browserVersion < 9))){
+                            if (buttonDom.getAttribute('disabled') !== 'disabled') {
+                                wrapper.removeClass('isFocused').removeClass('isPressed');
+                                isFocused = false;
+                            }
+                        }
+                        keyDown(e, e.keyCode);
+                    }
+                    function keyUp(e){
+                        if ((e.keyCode == 13) || (e.keyCode == 32)){
+                            e.preventDefault();
+                            e.stopPropagation();
+                            isKeyDown = false;
+                            if (!isPressed){
+                                wrapper.removeClass('isPressed');
+                            } else if (!isHover){
+                                wrapper.removeClass('isPressed');
+                            }
+                        }
+                    }
+
+//                    keypress/keyup
+                    if ($d.isOpera){
+                        wrapper.on('keypress', operaKeyPress);
+                    }
+                    else if ($d.isFx){
+                        wrapper.on('keypress', fxKeyPress);
+                    } else{
+                        wrapper.on('keydown', commonKeyDown);
+                    }
+                    wrapper.on('keyup', keyUp);
+
+                    if ($d.isTouch){
+
+//                    for touch
+                        wrapper.on('touchstart', function(e){
+                            e.preventDefault();
+                            if (buttonDom.getAttribute('disabled') !== 'disabled'){
+                                isPressed = true;
+                                if (!isFocused){
+                                    $this.focus();
+                                }
+                                wrapper.addClass( 'isPressed');
+                            }
+                        });
+                        $(window.document).on('touchend', function(e){
+                            if ((buttonDom.getAttribute('disabled') !== 'disabled') && isPressed){
+                                isPressed = false;
+                                removePress();
+                                action(e);
+                            }
+                        });
+
+                    } else{
+
+//                     for mouse
+                        wrapper.on({
+                            mousedown: function(e){
+                                e.preventDefault();
+                                if ((buttonDom.getAttribute('disabled') !== 'disabled') && (e.button != 2)){
+                                    isPressed = true;
+                                    if (!isFocused){
+                                        $this.focus();
+                                    }
+                                    wrapper.addClass('isPressed');
+                                }
+                            },
+                            mouseup: function(e){
+                                if ((buttonDom.getAttribute('disabled') !== 'disabled') && (e.button != 2)){
+                                    isPressed = false;
+                                    if (!isKeyDown){
+                                        removePress();
+                                        action(e);
+                                    }
+                                }
+                            }
+                        });
+
+                        $(window.document).on('mouseup', function(){
+                            isPressed = false;
+                        });
+//                     hover
+                        wrapper.on({
+                            mouseenter: function() {
+                                if (buttonDom.getAttribute('disabled') !== 'disabled') {
+                                    wrapper.addClass('isHover');
+                                    isHover = true;
+                                }
+                            },
+                            mouseleave: function() {
+                                wrapper.removeClass('isHover');
+                                isHover = false;
+                            }
                         });
                     }
-                },
-                mouseup: function() {
-                    wrapper.removeClass('isPressed');
-                    isPressed = false;
-                    action();
-                },
-//                focus
-                focus: function() {
-                    if ((buttonDom.getAttribute('disabled') !== 'disabled') && (!$d.isTouch)) {
-                        isFocused = true;
-                        wrapper.addClass('isFocused');
+
+                    $this.on('focus', focus);
+                    $this.on('blur', function (){
+                        if ($d.isIE && ($d.browserVersion < 9) && isHover){
+                            $this.focus();
+                            return false;
+                        }
+                        blur();
+                    });
+                });
+            },
+
+            disable: function(isDisable){
+                return this.each(function() {
+                    var $this = $(this),
+                        buttonDom = $(this)[0],
+                        wrapper = $this.parent();
+
+                    function disabled(isDisable){
+                        if (isDisable){
+                            buttonDom.setAttribute('disabled', 'disabled');
+                            wrapper.addClass('isDisabled')
+                                   .removeClass('isPressed')
+                                   .removeClass('isHover')
+                                   .removeClass('isFocused');
+                        } else{
+                            buttonDom.removeAttribute('disabled');
+                            wrapper.removeClass('isDisabled');
+                        }
                     }
-                },
-                blur: function(){
-                    wrapper.removeClass('isFocused').removeClass('isPressed');
-                    isFocused = false;
-                }
-            });
-            $this.on({
-                focus: function() {
-                    if ((buttonDom.getAttribute('disabled') !== 'disabled') && (!$d.isTouch)) {
-                        isFocused = true;
-                        wrapper.addClass('isFocused');
-                    }
-                },
-                blur: function(){
-                    wrapper.removeClass('isFocused').removeClass('isPressed');
-                    isFocused = false;
-                }
-            });
-//                keypress/keyup
-            if ($d.isOpera){
-                wrapper.on('keypress', operaKeyPress);
+
+                    disabled(isDisable);
+                });
             }
-            else if ($d.isFx){
-                wrapper.on('keypress', fxKeyPress);
-            } else{
-                wrapper.on('keydown', commonKeyDown);
-            }
-            wrapper.on('keyup', keyUp);
+        };
 
-        });
-    };
-
-    $.fn.setDisableButton = function(isDisable){
-        var isDisabled;
-
-        return this.each(function () {
-            var $this = $(this),
-                buttonDom = $(this)[0],
-                wrapper = $this.parent();
-
-            if(isDisable) {
-                buttonDom.setAttribute('disabled', 'disabled');
-                wrapper.addClass('isDisabled').removeClass('isPressed').removeClass('isHover').removeClass('isFocused');
-                isDisabled = true;
-            } else {
-                buttonDom.removeAttribute('disabled');
-                wrapper.removeClass('isDisabled');
-                isDisabled = false;
-            }
-        });
+    $.fn.button = function(method){
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            // error
+        }
     };
 })(jQuery);
