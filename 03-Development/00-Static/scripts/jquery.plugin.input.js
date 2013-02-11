9bef965e7950ef3161c50ff9ac0efe0806ad838a
Pavel Chuvashev <pavel.chuvashev@softwearfinance.com>
2013-02-11 18:43:37 +0400

    fixed input.

--- 03-Development/00-Static/scripts/jquery.plugin.input.js
+++ 03-Development/00-Static/scripts/jquery.plugin.input.js
@@ -4,62 +4,78 @@
  */
 
 (function($){
-    $.fn.input = function() {
+    var $d = $.$d,
+        methods = {
+            init: function() {
 
-        return this.each(function () {
-            var $this = $(this),
-                wrapper = $this.parent(),
-                trueInput = $this[0],
-                isPressed = false,
-                isFocused = false,
-                isHover = false;
+                return this.each(function () {
+                    var $this = $(this),
+                        wrapper = $this.parent(),
+                        trueInput = $this[0],
+                        isPressed = false,
+                        isFocused = false,
+                        isHover = false,
+                        isDisabled = false;
 
-            $this.on({
-//                hover
-                mouseenter: function() {
-                    if (!trueInput.disabled){
-                        isHover = true;
-                        wrapper.addClass('isHover');
-                    }
-                },
-                mouseleave: function() {
-                    isHover = false;
-                    wrapper.removeClass('isHover');
-                },
-//                focus
-                focus: function() {
-                    if (!trueInput.disabled){
-                        isFocused = true;
-                        wrapper.addClass('isFocused');
-                    }
-                },
-                blur: function() {
-                    isFocused = false;
-                    wrapper.removeClass('isFocused').removeClass('isPressed');
-                }
-            });
-//
-        });
-    };
+                    $this.on({
+        //                hover
+                        mouseenter: function() {
+                            if (!isDisabled){
+                                isHover = true;
+                                wrapper.addClass('isHover');
+                            }
+                        },
+                        mouseleave: function() {
+                            isHover = false;
+                            wrapper.removeClass('isHover');
+                        },
+        //                focus
+                        focus: function() {
+                            if (!isDisabled){
+                                isFocused = true;
+                                wrapper.addClass('isFocused');
+                            }
+                        },
+                        blur: function() {
+                            isFocused = false;
+                            wrapper.removeClass('isFocused').removeClass('isPressed');
+                        }
+                    });
 
-    $.fn.setDisable = function(isDisable){
-        var isDisabled;
+                    trueInput.setDisable = function(isDisable){
+                        if (isDisable !== isDisabled){
+                            if (isDisable){
+                                trueInput.setAttribute('disabled', 'disabled');
+                                wrapper.addClass('isDisabled')
+                                    .removeClass('isPressed')
+                                    .removeClass('isHover')
+                                    .removeClass('isFocused');
+                                isDisabled = true;
+                            } else{
+                                trueInput.removeAttribute('disabled');
+                                wrapper.removeClass('isDisabled');
+                                isDisabled = false;
+                            }
+                        }
+                    };
+//                    check default values
 
-        return this.each(function () {
-            var $this = $(this),
-                wrapper = $this.parent();
+                    trueInput.setDisable((trueInput.disabled === true) || (trueInput.getAttribute('disabled') === 'disabled'));
+                })
+            },
 
-            if(isDisable) {
-                $this.prop('disabled', true);
-                wrapper.addClass('isDisabled').removeClass('isPressed').removeClass('isHover').removeClass('isFocused');
-                isDisabled = true;
-            } else {
-                $this.prop('disabled', false);
-                wrapper.removeClass('isDisabled');
-                isDisabled = false;
+            disable: function(isDisable){
+                return this.each(function() {
+                    $(this)[0].setDisable(isDisable);
+                });
             }
+        };
 
-
-        });
+    $.fn.input = function(method){
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else {
+            return methods.init.apply(this, arguments);
+        }
     };
 })(jQuery);
